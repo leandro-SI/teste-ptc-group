e0a9e500c605aa2e122e0d613194582b380b2cf0
leandro-si <leandro.almeida@uvvnet.com.br>
2024-07-23 17:16:00 -0300

    criando controller base para retorno try catch

---
+++ Blog-PTC/BlogPTC.API/Base/BlogControllerBase.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BlogPTC.API.Base
+{
+    public abstract class BlogControllerBase : ControllerBase
+    {
+        [NonAction]
+        public virtual ObjectResult StatusCodeResponse(int statusCode, string message)
+        {
+            return this.StatusCode(statusCode, (object)new
+            {
+                message = new
+                {
+                    status = statusCode,
+                    message = message
+                }
+            });
+        }
+    }
+}
--- Blog-PTC/BlogPTC.API/Controllers/PostController.cs
+++ Blog-PTC/BlogPTC.API/Controllers/PostController.cs
@@ -1,4 +1,5 @@
-﻿using BlogPTC.Application.Dtos;
+﻿using BlogPTC.API.Base;
+using BlogPTC.Application.Dtos;
 using BlogPTC.Application.Interfaces;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
@@ -8,26 +9,35 @@ namespace BlogPTC.API.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    public class PostController : ControllerBase
+    public class PostController : BlogControllerBase
     {
         private readonly IPostService _postService;
+        private readonly ILogger _logger = null;
 
-        public PostController(IPostService postService)
+        public PostController(IPostService postService, ILogger<PostController> logger)
         {
             _postService = postService;
+            _logger = logger;
         }
 
         [Authorize(Roles = "Administrador, Usuario")]
         [HttpPost("new")]
         public async Task<IActionResult> Create([FromBody] NewPostDTO postDto)
         {
+            try
+            {
+                var userId = HttpContext.User.Claims.FirstOrDefault(u => u.Type == "user_id").Value;
+                postDto.UserId = userId;
 
-            var userId = HttpContext.User.Claims.FirstOrDefault(u => u.Type == "user_id").Value;
-            postDto.UserId = userId;
+                await _postService.CreatePost(postDto);
 
-            await _postService.CreatePost(postDto);
-
-            return Ok(new { mensagem = $"post {postDto.Title} criado com sucesso." });
+                return Ok(new { mensagem = $"post {postDto.Title} criado com sucesso." });
+            }
+            catch (Exception _erro)
+            {
+                _logger.LogError(_erro, _erro.Message, null);
+                return StatusCodeResponse(StatusCodes.Status500InternalServerError, "Erro ao cadastra novo post!");
+            }
         }
 
     }
